12e858ee95996bed3821f43042884fe8d071e08f
Antonio Teixeira <antoniolrt@gmail.com>
2017-09-27 14:53:55 -0300

    Deleted some unnecessary files

---
+++ config.h
@@ -0,0 +1,29 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `crypto' library (-lcrypto). */
+/* #undef HAVE_LIBCRYPTO */
+
+/* Name of package */
+#define PACKAGE "dersubj"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "antoniolrt@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "dersubj"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "dersubj 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "dersubj"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Version number of package */
+#define VERSION "0.1"
